2a4b41d6ddf8fa35a54b7924dc78c19da143f6c2
Kumaava <avaneesh.paper@gmail.com>
2016-09-15 00:03:39 +0530

    First commit

--- MyProj/.worksheet/src/MyDesign.scala
+++
@@ -1,3 +0,0 @@
-object MyDesign {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(61); 
-  println("Welcome to the Scala worksheet")}
-}
--- Practice/src/my/Practice/dog.scala
+++
@@ -1,5 +0,0 @@
-package my.Practice
-
-class dog {
-  
-}
